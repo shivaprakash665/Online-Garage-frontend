bf6cff0e6c453946660dd9a92e20be7c24835ed6
unknown <govindpoomukham@gmail.com>
2025-10-21 12:08:54 +0530

    login routes frnt

---
+++ src/component/dashboard/agent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AgentDashboard() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h2>Welcome, Insurance Agent!</h2>
+      <p>Manage your clients and insurance policies here.</p>
+    </div>
+  );
+}
+
+export default AgentDashboard;
